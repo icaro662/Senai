01abd3891da7e177e7fb8f8337e4f0465e866f60
icaro662 <icarosancho@gmail.com>
2026-09-08 10:33:17 -0300

    conteudo da aula 08.08

---
+++ setembro-2026/aula 08.09/script.js
@@ -0,0 +1,10 @@
+alert("careful... quiz incoming!")
+
+const quiz = prompt("who says that line, from hit tv show Supernatural?");
+
+if (quiz == "Lucifer" || "lucifer") {
+    alert("yep you got it");
+} else {
+    alert("bro... no... lol.");
+}
+
